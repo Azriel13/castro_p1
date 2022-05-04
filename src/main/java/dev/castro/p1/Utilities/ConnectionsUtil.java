141e16dc44e6eee6780be5339e577bf2f853fce2
Azriel13 <armorcrow@gmail.com>
2022-05-04 11:39:08 -0500

    testing github feature

--- src/main/java/dev/castro/p1/Utilities/ConnectionsUtil.java
+++ src/main/java/dev/castro/p1/Utilities/ConnectionsUtil.java
@@ -8,7 +8,7 @@ public class ConnectionsUtil {
     public static Connection createConnection(){
 
         try {
-            Connection conn = DriverManager.getConnection(System.getenv("p1_Postgres"));
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://castrodb.cd72nyeyabcf.us-east-2.rds.amazonaws.com/p1db?user=postgres&password=AzrielLucifer13");
             return conn;
         } catch (SQLException e) {
             e.printStackTrace();
