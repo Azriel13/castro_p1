84fd3b5ad40ced08c76bdcc215d9e932940edb34
Azriel13 <armorcrow@gmail.com>
2022-05-04 11:44:21 -0500

    testing github feature

--- src/test/java/dev/castro/p1/UtilitiesTests/LoggerTest.java
+++ src/test/java/dev/castro/p1/UtilitiesTests/LoggerTest.java
@@ -2,9 +2,11 @@ package dev.castro.p1.UtilitiesTests;
 
 import dev.castro.p1.Utilities.Logger;
 import jdk.jfr.internal.LogLevel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class LoggerTest {
+    @Disabled
     @Test
     void info_log_test(){
         Logger.log("Hello", LogLevel.INFO);
